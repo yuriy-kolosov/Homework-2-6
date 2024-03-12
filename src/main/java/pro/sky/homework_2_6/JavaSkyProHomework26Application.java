a4579833cc53d459abe7bd0e94f8d734d97f95f1
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-03-12 22:42:13 +0300

    Домашнее задание 2-6. Коллекции. Листы и сеты - начальный коммит

---
+++ src/main/java/pro/sky/homework_2_6/JavaSkyProHomework26Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_2_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkyProHomework26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkyProHomework26Application.class, args);
+	}
+
+}
