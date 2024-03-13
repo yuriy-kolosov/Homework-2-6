ecf629b8507f89abbadfd661cb50241b91334074
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-03-14 02:28:16 +0300

    Домашнее задание 2-6. Коллекции. Листы и сеты. Задача - веб-приложение учета сотрудников - редакция 3

--- src/main/java/pro/sky/homework_2_6/controllers/EmployeeController.java
+++ src/main/java/pro/sky/homework_2_6/controllers/EmployeeController.java
@@ -4,9 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pro.sky.homework_2_6.exceptions.EmployeeAlreadyAddedException;
-import pro.sky.homework_2_6.exceptions.EmployeeNotFoundException;
-import pro.sky.homework_2_6.exceptions.EmployeeStorageIsFullException;
 import pro.sky.homework_2_6.services.EmployeeService;
 
 @RestController
@@ -22,50 +19,24 @@ public class EmployeeController {
     @GetMapping(path = "/add")
     public String add(@RequestParam("firstName") String fName,
                       @RequestParam("lastName") String lName) {
-
-        try {
-            return employeeService.add(fName, lName);
-        } catch (EmployeeAlreadyAddedException e) {
-            throw new EmployeeAlreadyAddedException();
-        } catch (EmployeeStorageIsFullException e) {
-            throw new EmployeeStorageIsFullException();
-        }
-
+        return employeeService.add(fName, lName);
     }
 
     @GetMapping(path = "/find")
     public String find(@RequestParam("firstName") String fName,
                        @RequestParam("lastName") String lName) {
-
-        try {
-            return employeeService.find(fName, lName);
-        } catch (EmployeeNotFoundException e) {
-            throw new EmployeeNotFoundException();
-        }
-
+        return employeeService.find(fName, lName);
     }
 
     @GetMapping(path = "/remove")
     public String remove(@RequestParam("firstName") String fName,
                          @RequestParam("lastName") String lName) {
-
-        try {
-            return employeeService.remove(fName, lName);
-        } catch (EmployeeNotFoundException e) {
-            throw new EmployeeNotFoundException();
-        }
-
+        return employeeService.remove(fName, lName);
     }
 
     @GetMapping(path = "/browse")
     public String browse() {
-
-        try {
-            return employeeService.browseAll();
-        } catch (EmployeeNotFoundException e) {
-            throw new EmployeeNotFoundException();
-        }
-
+        return employeeService.browseAll();
     }
 
 }
